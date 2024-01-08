b5eb2b2212f987c0a6543e9fab1ebb5860beda29
mainflow <119511368+mainflow009@users.noreply.github.com>
2024-01-09 00:45:37 +0900

    Create 392. Is Subsequence.py

---
+++ leetcode/Two pointers/392. Is Subsequence.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        ptr1 = 0
+        ptr2 = 0
+        
+        if len(s) == 0: return True
+        
+        while ptr1 < len(s) and ptr2 < len(t):
+            if s[ptr1]==t[ptr2]:
+                ptr1 += 1
+            ptr2 += 1
+
+        if ptr1 == len(s): return True
