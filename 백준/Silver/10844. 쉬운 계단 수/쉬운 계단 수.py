a4f367fd6ac2e56b15e32cf1b144172d17a90cdd
mainflow <119511368+mainflow009@users.noreply.github.com>
2024-05-17 21:56:20 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+dp = [[0 for _ in range(10)] for _ in range(n)]
+dp[0] = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+try:
+    for i in range(1, n):
+        for j in range(10):
+            if j == 0:
+                dp[i][j] = dp[i - 1][j + 1]
+                continue
+
+            if j == 9:
+                dp[i][j] = dp[i - 1][j - 1]
+                continue
+
+            dp[i][j] = dp[i - 1][j - 1] + dp[i -1][j + 1]
+
+    print(sum(dp[n-1]) % 1000000000)
+except IndexError as e:
+    print(dp[1])
